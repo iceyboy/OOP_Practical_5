42d474d622bec91c7413cb0418d6e6e4340357ec
Ryan King <ryan.king2016@loretomilford.net>
2023-02-22 03:22:47 +0000

    Add files via upload

---
+++ AccountHolder.java
@@ -0,0 +1,37 @@
+package part01;
+
+public class AccountHolder { 
+	 private String name; 
+	 private String address; 
+	 private String phone; 
+	  
+	 public AccountHolder(String name, String address, String phone) { 
+	  this.name = name; 
+	  this.address = address; 
+	  this.phone = phone; 
+	 } 
+	 
+	 
+	 public String getName() {
+			return name;
+		}
+		
+		public String getAddress() {
+			return address;
+		}
+		
+		public String getPhone() {
+			return phone;
+		}
+		
+		public String toString() {
+			
+			String result = "";
+			result += name + ", ";
+			result += address + ", ";
+			result += phone;
+			return result;
+			
+		}
+	  
+	} 
