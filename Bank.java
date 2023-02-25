11e7603a0be5a48843e48cfbc20f27e9a65ec910
Ryan King <ryan.king2016@loretomilford.net>
2023-02-25 03:51:02 +0000

    Add files via upload

--- Bank.java
+++ Bank.java
@@ -3,36 +3,50 @@ package part01;
 import java.util.ArrayList;
 
 public class Bank {
+	
 	private String name;
+	
 	private String address;
+	
 	private ArrayList<BankAccount> collection;
 
 	public Bank(String name, String address) {
+		
 		this.name = name;
+		
 		this.address = address;
+		
 		this.collection = new ArrayList<BankAccount>();
+		
 	}
 
 	public String getName() {
+		
 		return this.name;
+		
 	}
 
 	public String getAddress() {
-		return this.address;
-	}
-	
-	public ArrayList<BankAccount> getBankAccts(){
 		
-		return this.collection;
+		return this.address;
 		
 	}
 
+	public ArrayList<BankAccount> getBankAccts() {
+
+		return this.collection;
+
+	}
+
 	public void openNewAccount(String name, String address, String phone, double startingBalance) {
+		
 		BankAccount ba = new BankAccount(name, address, phone, startingBalance);
+		
 		this.collection.add(ba);
 	}
 
 	public String toString() {
+		
 		String result = "Accounts within " + name + " are as follows:" + "\n" + "\n";
 
 		for (int i = 0; i < collection.size(); i++) {
