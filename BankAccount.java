2dce6d7bd7aac5074639d7488fa81ffda23888c6
Ryan King <ryan.king2016@loretomilford.net>
2023-02-25 03:51:09 +0000

    Add files via upload

--- BankAccount.java
+++ BankAccount.java
@@ -1,135 +1,155 @@
 package part01;
 
-public class BankAccount { 
-	 private int acctNum; 
-	 private static int nextAcctNum = 1; 
-	 private AccountHolder owner; 
-	 private double balance;
+public class BankAccount {
+	
+	private int acctNum;
+	
+	private static int nextAcctNum = 1;
+	
+	private AccountHolder owner;
+	
+	private double balance;
+	
 	private String name;
+	
 	private String address;
-	private String phone; 
-	 
-	 public BankAccount(String name, String address, String phone) { 
-	  // call other constructor 
-	  this(name, address, phone, 0.0); 
-	 } 
-	 
-	 public BankAccount(String name, String address, String phone, double 
-	startingBalance) { 
-	  // create and assign AccountHolder 
-	  this.owner = new AccountHolder(name, address, phone); 
-	 
-	  // set account number 
-	  this.acctNum = nextAcctNum; 
-	  nextAcctNum++; 
-	  
-	 
-	  // set account balance 
-	  this.balance = startingBalance; 
-	 } 
-	 
-	 public BankAccount() {
+	
+	private String phone;
 
-			this(null, null, null, -1);
+	public BankAccount(String name, String address, String phone) {
+		
+		this(name, address, phone, 0.0);
+		
+	}
 
-		}
-	 
-	 public void setName(String name) {
+	public BankAccount(String name, String address, String phone, double startingBalance) {
+		
+		this.owner = new AccountHolder(name, address, phone);
+		
+		this.acctNum = nextAcctNum;
+		
+		nextAcctNum++;
 
-			if (name != null) {
+		
+		this.balance = startingBalance;
+		
+	}
 
-				this.name = name;
+	public BankAccount() {
 
-			} else {
+		this(null, null, null, -1);
 
-				if (this.name == null) {
+	}
 
-					this.name = "Unknown";
+	public void setName(String name) {
 
-				}
+		if (name != null) {
+
+			this.name = name;
+
+		} else {
+
+			if (this.name == null) {
+
+				this.name = "Unknown";
 
 			}
 
 		}
 
-		public void setAddress(String address) {
+	}
 
-			if (address != null) {
+	public void setAddress(String address) {
 
-				this.address = address;
+		if (address != null) {
 
-			} else {
+			this.address = address;
 
-				if (this.address == null) {
+		} else {
 
-					this.address = "Unknown";
+			if (this.address == null) {
 
-				}
-
-			}
-
-		}
-
-		public void setPhone(String phone) {
-
-			if (phone != null) {
-
-				this.phone = phone;
-
-			} else {
-				if (this.phone == null) {
-
-					this.phone = "Unknown";
-
-				}
-			}
-
-		}
-		
-		public void setBalance(double balance) {
-
-			if (balance >= 0) {
-
-				this.balance = balance;
-
-			} else {
-
-				this.balance = -1;
+				this.address = "Unknown";
 
 			}
 
 		}
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 public int getAcctNum() {
-			return acctNum;
-		}
-		
-		public AccountHolder getOwner() {
-			return owner;
-		}
-		
-		public double getBalance() {
-			return balance;
-		}
-		
-		public String toString() {
+
+	}
+
+	public void setPhone(String phone) {
+
+		if (phone != null) {
+
+			this.phone = phone;
+
+		} else {
 			
-			
-			String result = "";
-			result += "Acct No: " + acctNum + "\n";
-			result += "Customer: " + owner.toString() + "\n";
-			result += "Balance: " + balance + "\n";
-			return result;
-			
+			if (this.phone == null) {
+
+				this.phone = "Unknown";
+
+			}
 		}
+
+	}
+
+	public void setBalance(double balance) {
+
+		if (balance >= 0) {
+
+			this.balance = balance;
+
+		} else {
+
+			this.balance = -1;
+
+		}
+
+	}
+	
+	public void deposit(double amount) {
 		
+		this.balance = balance + amount;
 		
-	 
-	} 
+	}
+	
+	public void withdraw(double amount) {
+		
+		this.balance = balance - amount;
+		
+	}
+
+	public int getAcctNum() {
+		
+		return acctNum;
+		
+	}
+
+	public AccountHolder getOwner() {
+		
+		return owner;
+		
+	}
+
+	public double getBalance() {
+		
+		return balance;
+		
+	}
+
+	public String toString() {
+
+		String result = "";
+		
+		result += "Acct No: " + this.acctNum + "\n";
+		
+		result += "Customer: " + this.owner.toString() + "\n";
+		
+		result += "Balance: " + this.balance + "\n";
+		
+		return result;
+
+	}
+
+}
