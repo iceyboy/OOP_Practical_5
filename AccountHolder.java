27f88e0b0a7d5bb3e98dd1dcd869377098f9c06a
Ryan King <ryan.king2016@loretomilford.net>
2023-02-25 03:50:54 +0000

    Add files via upload

--- AccountHolder.java
+++ AccountHolder.java
@@ -1,37 +1,53 @@
 package part01;
 
-public class AccountHolder { 
-	 private String name; 
-	 private String address; 
-	 private String phone; 
-	  
-	 public AccountHolder(String name, String address, String phone) { 
-	  this.name = name; 
-	  this.address = address; 
-	  this.phone = phone; 
-	 } 
-	 
-	 
-	 public String getName() {
-			return name;
-		}
+public class AccountHolder {
+	
+	private String name;
+	
+	private String address;
+	
+	private String phone;
+
+	public AccountHolder(String name, String address, String phone) {
 		
-		public String getAddress() {
-			return address;
-		}
+		this.name = name;
 		
-		public String getPhone() {
-			return phone;
-		}
+		this.address = address;
 		
-		public String toString() {
-			
-			String result = "";
-			result += name + ", ";
-			result += address + ", ";
-			result += phone;
-			return result;
-			
-		}
-	  
-	} 
+		this.phone = phone;
+		
+	}
+
+	public String getName() {
+		
+		return name;
+		
+	}
+
+	public String getAddress() {
+		
+		return address;
+		
+	}
+
+	public String getPhone() {
+		
+		return phone;
+		
+	}
+
+	public String toString() {
+
+		String result = "";
+		
+		result += this.getName() + ", ";
+		
+		result += this.getAddress() + ", ";
+		
+		result += this.getPhone();
+		
+		return result;
+
+	}
+
+}
