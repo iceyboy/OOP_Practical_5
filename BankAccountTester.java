86263b1747eee4aaca4165d7c07a6a94c587e9a4
Ryan King <ryan.king2016@loretomilford.net>
2023-02-25 03:51:17 +0000

    Add files via upload

--- BankAccountTester.java
+++ BankAccountTester.java
@@ -3,11 +3,13 @@ package part01;
 public class BankAccountTester {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 			
 		BankAccount banker = new BankAccount("Ryan King", "Portlean", "0838320765", 100000.00);
+		
 		System.out.println(banker);
+		
 		BankAccount banker1 = new BankAccount("Ryan King2", "Portlean", "0838320765", 100000.00);
+		
 		System.out.println(banker1);
 		
 	}
